44f3f90b3e7a0c5d38ad5ce98ba5d9a13a9d1fee
Bartosz <bartosz.burdalski@gmail.com>
2024-12-01 17:56:16 +0100

    feat: Update server actions

--- src/actions/navs.ts
+++ src/actions/navs.ts
@@ -3,20 +3,29 @@
 import { Nav, TreeItems } from "@/types";
 import { redirect } from "next/navigation";
 
-export async function createNav(items: TreeItems) {
+export async function createNav(items: TreeItems, id?: string) {
   await fetch("http://localhost:3000/api/nav", {
     method: "POST",
-    body: JSON.stringify(items),
+    body: JSON.stringify({ items, id }),
     next: { tags: ["nav"] },
   });
   redirect("/");
 }
 
 export async function getNavs() {
-  const res = await fetch("http://localhost:3000/api/nav", {
+  const response = await fetch("http://localhost:3000/api/nav", {
     next: { tags: ["nav"] },
   });
-  const data = (await res.json()) as Nav[];
+  const data = (await response.json()) as Nav[];
+
+  return data;
+}
+
+export async function getNavById(id: string) {
+  const response = await fetch(`http://localhost:3000/api/nav/${id}`, {
+    next: { tags: ["nav"] },
+  });
+  const data = (await response.json()) as Nav;
 
   return data;
 }
